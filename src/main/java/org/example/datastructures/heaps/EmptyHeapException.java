733c907d1a4e4a09a734f91bbbae7c542c82a92a
jjakubia <jakubiakrjulian@gmail.com>
2024-02-29 10:00:14 +0100

    reduce size of dummy project

--- src/main/java/org/example/datastructures/heaps/EmptyHeapException.java
+++
@@ -1,9 +0,0 @@
-package org.example.datastructures.heaps;
-
-@SuppressWarnings("serial")
-public class EmptyHeapException extends Exception {
-
-    public EmptyHeapException(String message) {
-        super(message);
-    }
-}
